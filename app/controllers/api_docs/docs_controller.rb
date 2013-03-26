36e4088b1d74f8e907f646fcec9e40961bb04463
Aaron Price <price.aaron@gmail.com>
2013-03-26 15:32:14 -0400

    Allow separation of API versions.

--- app/controllers/api_docs/docs_controller.rb
+++ app/controllers/api_docs/docs_controller.rb
@@ -1,7 +1,7 @@
 class ApiDocs::DocsController < ApiDocs.config.base_controller.to_s.constantize
   def index
     @api_controllers = { }
-    Dir.glob(ApiDocs.config.docs_path.join('*.yml')).each do |file_path|
+    Dir.glob(ApiDocs.config.docs_path.join("#{params[:version]}*.yml")).each do |file_path|
       @api_controllers[File.basename(file_path, '.yml')] = YAML.load_file(file_path)
     end
   end
--- test/docs_controller_test.rb
+++ test/docs_controller_test.rb
@@ -1,11 +1,35 @@
 require File.expand_path('test_helper', File.dirname(__FILE__))
 
 class ApiDocs::DocsControllerTest < ActionController::TestCase
+
+  def setup
+    File.open(ApiDocs.config.docs_path.join('v1:test.yml'), 'w'){|f| f.write({}.to_yaml)}
+  end
+
+  def teardown
+    Dir.glob(ApiDocs.config.docs_path.join('*.yml')).each do |file_path|
+      FileUtils.rm(file_path)
+    end
+  end
   
   def test_get_index
     get :index
     assert_response :success
     assert assigns(:api_controllers)
+    assert_template :index
+  end
+
+  def test_get_index_with_version
+    get :index, version: "v1"
+    assert_response :success
+    assert_equal assigns(:api_controllers), {"v1:test"=>{}}
+    assert_template :index
+  end
+
+  def test_get_index_with_non_existant_version
+    get :index, version: "v2"
+    assert_response :success
+    assert_equal assigns(:api_controllers), {}
     assert_template :index
   end
 end
