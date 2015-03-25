fe95e997176553c7eeee054cfcc4ce6f22807773
Aaron Price <aprice@flipgive.com>
2015-03-25 16:24:21 -0400

    Page should 404 when no doc found.

--- app/controllers/api_docs/docs_controller.rb
+++ app/controllers/api_docs/docs_controller.rb
@@ -4,5 +4,6 @@ class ApiDocs::DocsController < ApiDocs.config.base_controller.to_s.constantize
     Dir.glob(ApiDocs.config.docs_path.join("#{params[:version]}*.yml")).each do |file_path|
       @api_controllers[File.basename(file_path, '.yml')] = YAML.load_file(file_path)
     end
+    raise ActionController::RoutingError.new('Not Found') if @api_controllers.blank?
   end
 end
